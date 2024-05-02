ac388d68033a88c528fe5c64c4015d317d1ae714
joaonative <jestudos127@gmail.com>
2024-05-02 10:43:41 -0300

    feat: footer component

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary/25 py-2">
+      <h1 className="text-base text-center">
+        {" "}
+        Made & Designed by{" "}
+        <a
+          className="font-bold"
+          href="https://github.com/joaonative"
+          target="_blank"
+        >
+          @joaonative
+        </a>
+      </h1>
+    </footer>
+  );
+};
+
+export default Footer;
